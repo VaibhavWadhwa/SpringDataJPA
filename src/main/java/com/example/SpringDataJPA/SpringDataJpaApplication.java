bfd8c317a39c75f2a933dc8b55b33ea7026bd436
vaibhav.wadhwa <vaibhav.wadhwa@newgensoft.com>
2025-08-02 13:13:30 +0530

    added postgres crud operations.

--- src/main/java/com/example/SpringDataJPA/SpringDataJpaApplication.java
+++ src/main/java/com/example/SpringDataJPA/SpringDataJpaApplication.java
@@ -1,13 +1,33 @@
 package com.example.SpringDataJPA;
 
+import com.example.SpringDataJPA.model.Student;
+import com.example.SpringDataJPA.repo.StudentRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDataJpaApplication.class, args);
+
+	ApplicationContext context = SpringApplication.run(SpringDataJpaApplication.class, args);
+		StudentRepo repo = context.getBean(StudentRepo.class);
+
+//		Student s1 = context.getBean(Student.class);
+//
+//		s1.setName("Vaibhav");
+//		s1.setMarks(99);
+//		s1.setRollNo(101);
+
+		List<Student> list = repo.findAll();
+		System.out.println(list);
+
+//		repo.save(s1);
+
+
 	}
 
 }
